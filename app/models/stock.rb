b435c6a676ae865d26157c67304332e087b22ded
Alex <info@ezwebsupport.com>
2018-06-16 21:28:32 +0000

    user stocks assoc

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,4 +1,7 @@
 class Stock < ActiveRecord::Base
+  has_many :user_stocks
+  has_many :users, through: :user_stocks
+
   def self.look_up(ticker)
     begin
       stock = StockQuote::Stock.quote(ticker)
@@ -6,5 +9,9 @@ class Stock < ActiveRecord::Base
     rescue Exception => e
       return nil
     end
+  end
+  
+  def self.find_by_ticker(ticker_symbol)
+    where(ticker: ticker_symbol).first
   end
 end
