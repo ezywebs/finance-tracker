df266737c085eae9f20b5aa524630d9b44795937
Alex <info@ezwebsupport.com>
2018-06-24 04:45:56 +0000

    finance app almost ready

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,12 @@ Rails.application.routes.draw do
   root 'users#my_portfolio'
   get 'my_portfolio', to: 'users#my_portfolio'
   get 'search_stocks', to: 'stocks#search'
+  get 'my_friends', to: 'users#my_friends'
   resources :user_stocks, only: [:create, :destroy]
+  resources :users, only: [:show]
+  resources :friendships
+  get 'search_friends', to: 'users#search'
+  post 'add_friend', to: 'users#add_friend'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
